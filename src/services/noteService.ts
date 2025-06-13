7fd8760dbef11214dc31ef2affa0f98fbbb175db
ToriRuban <toriruban@icloud.com>
2025-06-13 14:38:19 +0200

    fix: noteService

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -10,31 +10,35 @@ export interface FetchNotesResponse {
     page: number,
     perPage: number,
 }
+export interface CreateNote {
+    title: string;
+    content: string;
+    tag: 'Work' | 'Personal' | 'Meeting' | 'Shopping' | 'Todo';
+  }
 
 export const fetchNotes = async (search: string,  page: number, perPage: number): Promise<FetchNotesResponse> => {
    const params: { page: number, perPage: number, search?: string } = {
     page, perPage
    };
    if (search){
-    params.search = search
+    params.search = search;
    }
     const response = await axios.get<FetchNotesResponse>(`${ BASE_URL }/notes`, {
-    params: params,
+    params,
     headers: { Authorization: `Bearer ${ API_TOKEN }` }
    })
-
    return response.data;
 }
 
-export const createNote = async (newNote: NoteTag) => {
+export const createNote = async (newNote: CreateNote): Promise<Note> => {
     const response = await axios.post<Note>(`${ BASE_URL }/notes/`, newNote, {
         headers: { Authorization: `Bearer ${ API_TOKEN }` }
     })
     return response.data;
 }
 
-export const deleteNote = async (noteId: number) => {
-    const response = await axios.delete(`${ BASE_URL }/notes/${ noteId }`,{
+export const deleteNote = async (noteId: number): Promise<Note> => {
+    const response = await axios.delete<Note>(`${ BASE_URL }/notes/${ noteId }`,{
         headers: { Authorization: `Bearer ${ API_TOKEN }` }
     })
     return response.data;
